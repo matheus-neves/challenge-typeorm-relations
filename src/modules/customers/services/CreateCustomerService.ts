e3677de7af02264152bdafcead8f80456ce9db58
Matheus Neves <matheusneves1989@gmail.com>
2020-06-15 18:26:07 -0300

    feat(customers): controller and service implementation

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,21 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const checkEmailExist = await this.customersRepository.findByEmail(email);
+
+    if (checkEmailExist) {
+      throw new AppError('User already exists.');
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
   }
 }
 
